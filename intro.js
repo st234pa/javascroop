2e7493fb3d546c316992c73e37b0962ebc89da0e
st234pa <syoon1@stuy.edu>
2016-12-16 14:33:54 -0500

    we'll see lol

--- intro.js
+++ intro.js
@@ -1,12 +1,29 @@
-var changeList = function(e, i) {
+/*var changeList = function(e, i) {
     thelist = document.getElementById("thelist");
-    if (e === "click") {
+    if (e.type === "mousedown") {
 	thelist[i].remove();
     }
-    else if (e === "") {
+    else if (e.type === "") {
     }
     
 };
 
 var getIndex = function(e) {
+*/
+
+removeEle = function(i) {
+    thelist[i].remove();
+};
+
+changeHead = function(i) {
+    var head = document.getElementbyId("h");
+    head.innerHTML = thelist[i];
+};
+
+var thelist = document.getElementsbyTagName("li");
+for (int i = 0; i < thelist.length; i++) {
+    thelist[i].addEventListener("click", removeEle);
+    thelist[i].addEventListener("mouseover", changeHead);
+}
+
     
